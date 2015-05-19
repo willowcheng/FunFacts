e163d652eb4ff8bfb6a9d6ab371920cd3803f294
willowcheng <e.wingus@Gmail.com>
2015-05-19 10:32:29 -0400

    Recap of swift basics

---
+++ Scratchpad.playground/Contents.swift
@@ -0,0 +1,21 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+//Swift Recap
+
+var someVariable = "aVariable"
+let someConstant = 20
+
+someVariable = "anotherString"
+
+var fruitsArray = ["apples"]
+fruitsArray.append("banana")
+
+fruitsArray
+
+//Optional
+var optionalString: String? = "hello"
+optionalString!
