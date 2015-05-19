ac92e237ad8bf1dd0842b444a100d8e232a8544c
willowcheng <e.wingus@Gmail.com>
2015-05-19 11:05:23 -0400

    Add IBAction and IBOutlet

--- FunFacts/ViewController.swift
+++ FunFacts/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var funFactLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +21,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func showFunFact() {
+        funFactLabel.text = "Another interesting fact"
+    }
 
 }
 
