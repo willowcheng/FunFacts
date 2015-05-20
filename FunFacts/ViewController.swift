27d3d53e367bf41cec20af51ae33e3e5b71af816
willowcheng <e.wingus@Gmail.com>
2015-05-20 11:02:09 -0400

    Add a pop of color

--- FunFacts/ViewController.swift
+++ FunFacts/ViewController.swift
@@ -11,8 +11,10 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var funFactLabel: UILabel!
+    @IBOutlet weak var funFactButton: UIButton!
     
     let factBook = FactBook()
+    let colorWheel = ColorWheel()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,6 +28,9 @@ class ViewController: UIViewController {
     }
 
     @IBAction func showFunFact() {
+        var randomColor = colorWheel.randomColor()
+        view.backgroundColor = randomColor
+        funFactButton.tintColor = randomColor
         funFactLabel.text = factBook.randomFact()
     }
 
--- Scratchpad.playground/Contents.swift
+++ Scratchpad.playground/Contents.swift
@@ -24,3 +24,8 @@ optionalString!
 
 var randomNumber = Int(arc4random_uniform(10))
 
+//UIColor
+
+var redColor = UIColor(red: 223/255.0, green: 86/255.0, blue: 94/255.0, alpha: 1.0)
+
+let colorsArray = [redColor]
